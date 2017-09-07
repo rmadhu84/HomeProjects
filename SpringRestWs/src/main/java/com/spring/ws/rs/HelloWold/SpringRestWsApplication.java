e3325e380b94e9b64e2685041bf41d5a4acc3a57
Madhu <Madhu@madhusudransmbp.attlocal.net>
2017-09-07 05:21:53 -0400

    Initial check in.

---
+++ SpringRestWs/src/main/java/com/spring/ws/rs/HelloWold/SpringRestWsApplication.java
@@ -0,0 +1,12 @@
+package com.spring.ws.rs.HelloWold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWsApplication.class, args);
+	}
+}
